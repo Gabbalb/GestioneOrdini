d81229e75441b65b8ca5f3b2bcfd83e93404cf65
Gabriele Balbiani <balbiani.gabriele@gmail.com>
2022-08-16 15:23:59 +0200

    Add menu

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
+void menu (){
+    cout << "----------Gestione ordini----------" << endl;
+    cout << "-----------Pincopallotek-----------" << endl;
+    cout << "1) Crea nuovo ordine" << endl;
+    cout << "2) Modifica nuovo ordine" << endl;
+    cout << "3) Mostra lista ordini" <<endl;
+}
+
 int main() {
+
+    menu();
 
     return 0;
 }
